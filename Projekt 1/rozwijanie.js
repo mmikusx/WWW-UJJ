29c0a284e10c6e4ce697a40436d06adf1c224817
Mikolaj <mikolajski007@gmail.com>
2022-10-25 18:00:55 +0200

    Initial commit

---
+++ Projekt 1/rozwijanie.js
@@ -0,0 +1,41 @@
+const navZmianaKoloru = () => {
+    const navbar = document.querySelector('nav');
+    window.onscroll = () => {
+        if (window.scrollY > 665) {
+            navbar.classList.add('nav-active-zmiana-koloru');
+        } else {
+            navbar.classList.remove('nav-active-zmiana-koloru');
+        }
+    };
+}
+
+const navRozwin = () => {
+    const rozwin = document.querySelector('.rozwin');
+    const nav = document.querySelector('.nav-links');
+    const linki = document.querySelectorAll('.nav-links li')
+
+    // kiedy klikniemy w rozwin nasza nav zmieni sie na nav-active
+    rozwin.addEventListener('click', ()=> {
+        nav.classList.toggle('nav-active-rozwijanie');
+
+        //animate links
+        linki.forEach((linkk, index) => {
+            if (linkk.style.animation) {
+                linkk.style.animation = ``;
+            } else {
+                linkk.style.animation = `navRozwijanie 0.2s ease forwards ${index / 7 + 0.6}s`;
+            }
+        });
+
+        rozwin.classList.toggle('toggle');
+    });
+}
+
+const app = () => {
+    navZmianaKoloru();
+    navRozwin(); //wykonaj funkcje
+}
+
+app();
+
+// # TODO naprawic zmiane na zielony jak sie nie wylaczy menu a zmieni szerokosc na wieksza
